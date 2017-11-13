3b9db0890d85347961e1045d83e1320d265616c1
Andres Mauricio Rondon Patino <amrondonp@gmail.com>
2017-11-12 21:33:22 -0500

    PCP initial implementation

--- final-project/preprocessing/pitch_class_profiling.py
+++ final-project/preprocessing/pitch_class_profiling.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
+import numpy as np
 from scipy.io import wavfile
-from scipy.fftpack import rfft
+from scipy.fftpack import fft
+from math import log2
 
 class PitchClassProfiler():
     def __init__(self, file_name):
@@ -22,7 +24,7 @@ class PitchClassProfiler():
         return self._samples
 
     def fourier(self):
-        return rfft(self.samples())
+        return fft(self.samples())
 
     def plot_signal(self):
         plt.plot(self.samples())
@@ -33,4 +35,44 @@ class PitchClassProfiler():
         plt.show()
 
     def get_profile(self):
-        pass
+        #The algorithm here is implemented using
+        #the names of the math formula as shown in the paper
+
+        fs = self.frecuency()
+        X = self.fourier()
+        fref = [16.35, 17.32, 18.35, 19.45, 20.60, 21.83, 23.12, 24.50, 25.96, 27.50, 29.14, 30.87]
+
+        N = len(X)
+        assert(N % 2 == 0)
+
+        def M(l, p):
+            if l == 0:
+                return -1
+            return round(12 * log2( (fs * l)/(N * fref[p] )  ) ) % 12
+
+        pcp = [0 for p in range(12)]
+        
+        for p in range(12):
+            for l in range(N//2):
+                if p == M(l, p):
+                    pcp[p] += abs(X[l])**2
+        
+        #Normalize pcp
+        pcp_norm = [0 for p in range(12)]
+        for p in range(12):
+            pcp_norm[p] = (pcp[p] / sum(pcp))
+
+        return pcp_norm
+
+    def plot_profile(self):
+        objects = ('C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B')
+        y_pos = np.arange(len(objects))
+        performance = self.get_profile()
+        
+        plt.bar(y_pos, performance, align='center', alpha=0.5)
+        plt.xticks(y_pos, objects)
+        plt.ylabel('Energy')
+        plt.title('PCP results')
+        
+        plt.show()
+
--- final-project/test.py
+++ final-project/test.py
@@ -3,5 +3,7 @@ from preprocessing.pitch_class_profiling import PitchClassProfiler
 CHORDS_FOLDER = "C:/Users/amron/Downloads/jim2012Chords/Guitar_Only/"
 
 pitch_class_profiler = PitchClassProfiler(CHORDS_FOLDER + "e/e1.wav")
-pitch_class_profiler.plot_signal()
-#pitch_class_profiler.plot_fourier()
+
+pitch_class_profiler.plot_profile()
+
+
