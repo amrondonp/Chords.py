bd1b544fcbfcf926421f436707a0945a1f0f4f5a
Andres Mauricio Rondon Patino <amrondonp@gmail.com>
2017-11-12 22:43:02 -0500

    Generate files

--- final-project/test.py
+++ final-project/test.py
@@ -1,9 +1,19 @@
+import sys
 from preprocessing.pitch_class_profiling import PitchClassProfiler
 
 CHORDS_FOLDER = "C:/Users/amron/Downloads/jim2012Chords/Guitar_Only/"
 
-pitch_class_profiler = PitchClassProfiler(CHORDS_FOLDER + "e/e1.wav")
-
-pitch_class_profiler.plot_profile()
+with open("c.csv", "w") as f: 
+    for i in range(1,6):
+        name = "c/c" + str(i) + ".wav"
+        print(name)
+        pitch_class_profiler = PitchClassProfiler(CHORDS_FOLDER + name)
+        profile = pitch_class_profiler.get_profile()
+        for j in range(12):
+            f.write(str(profile[j]))
+            if( j < 11):
+                f.write(", ")
+            else:
+                f.write("\n")
 
 
