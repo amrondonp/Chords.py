4e42cf793539ad2991e39874465a2e525fa291a2
Andres Mauricio Rondon Patino <amrondonp@gmail.com>
2017-11-12 19:37:59 -0500

    PCP layout

---
+++ final-project/preprocessing/pitch_class_profiling.py
@@ -0,0 +1,30 @@
+from scipy.io import wavfile
+from scipy.fftpack import rfft
+
+class PitchClassProfiler():
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.read = False
+
+    def _read_file(self):
+        self._frecuency, self._samples = wavfile.read(self.file_name)
+        self.read = True
+
+    def frecuency(self):
+        if not self.read:
+            self._read_file()
+        return self._frecuency
+
+    def samples(self):
+        if not self.read:
+            self._read_file()
+        return self._samples
+
+    def fourier(self):
+        return rfft(self.samples())
+
+    def plot_signal(self):
+        
+
+    def get_profile(self):
+        print(self.fourier())
---
+++ final-project/test.py
@@ -0,0 +1,5 @@
+from preprocessing.pitch_class_profiling import PitchClassProfiler
+
+pitch_class_profiler = PitchClassProfiler("C:\\Users\\amron\\Downloads\\jim2012Chords\\Guitar_Only\\a\\a99.wav")
+pitch_class_profiler.get_profile()
+
