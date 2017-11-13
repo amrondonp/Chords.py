a984d68cee250e11f396181e5cd3b8c9985f4b75
Andres Mauricio Rondon Patino <amrondonp@gmail.com>
2017-11-13 18:39:12 -0500

    Refactor csv regeration phase 1

--- final_project/preprocessing/csv_generator.py
+++ final_project/preprocessing/csv_generator.py
@@ -1,9 +1,37 @@
 import os
 
+from io import StringIO
 from preprocessing.pitch_class_profiling import PitchClassProfiler
 from util import config
 
+CHORDS_FOLDER = config()["chords_path"] 
+
 class CsvGenerator():
     def __init__(self):
-        self.instruments = 
-        print(config()["dataset_path"])
+        pass
+
+    def profile_to_csv_string(self, profile):
+        output = ""
+        for j in range(12):
+            output += str(profile[j])
+            if( j < 11):
+                output += (", ")
+            else:
+                output += ("\n")
+        return output
+
+    def generate_csv(self, pitch):
+        with open("dataset/train/" + pitch + ".csv", "w") as f: 
+            for i in range(1,201):
+                name = "/" + pitch + "/" + pitch + str(i) + ".wav"
+                print(name)
+                pitch_class_profiler = PitchClassProfiler(CHORDS_FOLDER + name)
+                profile = pitch_class_profiler.get_profile()
+                f.write(self.profile_to_csv_string(profile))
+                
+    def generate_training_dataset(self):
+        print("Generating data... This may take several minutes")
+        for pitch in config()["pitches"]:
+            print("Generating data for pitch " + pitch)
+            self.generate_csv(pitch)
+
--- final_project/test.py
+++ final_project/test.py
@@ -4,25 +4,6 @@ from util import config
 
 CHORDS_FOLDER = config()["chords_path"] 
 
-def generate_csv(pitch):
-    with open("dataset/" + pitch + ".csv", "w") as f: 
-        for i in range(1,201):
-            name = "/" + pitch + "/" + pitch + str(i) + ".wav"
-            print(name)
-            pitch_class_profiler = PitchClassProfiler(CHORDS_FOLDER + name)
-            profile = pitch_class_profiler.get_profile()
-            for j in range(12):
-                f.write(str(profile[j]))
-                if( j < 11):
-                    f.write(", ")
-                else:
-                    f.write("\n")
-
-def plot_test(file):
-    pitch_class_profiler = PitchClassProfiler(CHORDS_FOLDER + "/" + file)
-    print(pitch_class_profiler.get_profile())
-    pitch_class_profiler.plot_profile()
-
 #plot_test("c/c50.wav")
 #generate_csv("em")
 #https://ccrma.stanford.edu/~joshua79/220a/final-proj.html
@@ -32,12 +13,13 @@ trainer = Trainer()
 #trainer.save()
 
 
-trainer.train()
+#trainer.train()
 #prediction = trainer.predict(CHORDS_FOLDER + "/a/a2.wav")
 #print(prediction.tolist()[0])
 #trainer.plot_prediction(CHORDS_FOLDER + "/g/gMauro.wav")
 
 #trainer.plot_prediction("C:/Users/amron/Downloads/jim2012Chords/Other_Instruments/Piano/bm/bm2.wav")
 
-#from preprocessing.csv_generator import CsvGenerator
-#a = CsvGenerator()
+from preprocessing.csv_generator import CsvGenerator
+a = CsvGenerator()
+a.generate_training_dataset()
