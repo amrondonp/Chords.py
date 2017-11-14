89d4f8a40cc092d578c27f958ea1dd74ef3d5393
Andres Mauricio Rondon Patino <amrondonp@gmail.com>
2017-11-13 23:27:59 -0500

    Add more songs

--- final_project/test.py
+++ final_project/test.py
@@ -1,27 +1,32 @@
 import sys
 from util import config
 from neural_network.classifier import Classifier, Trainer
-from preprocessing.pitch_class_profiling import LongFileProfiler
+from preprocessing.pitch_class_profiling import LongFileProfiler, PitchClassProfiler
 
-# classifier = Classifier()
-# file_path = sys.argv[1] if len(sys.argv) > 1 else "hand-made-chords/c.wav"
-# classifier.classify(file_path)
+classifier = Classifier()
+file_path = sys.argv[1] if len(sys.argv) > 1 else "songs/d.wav"
+classifier.classify(file_path)
 
-import numpy as np
 
-trainer = Trainer()
-trainer.load()
+#profiler = PitchClassProfiler(file_path)
+#profiler.plot_profile()
 
-song = "escala.wav"
 
-longFileProfiler = LongFileProfiler("songs/" + song)
-profiles = longFileProfiler.get_profile()
+# import numpy as np
 
-with open("songs_chords/" + song.split(".")[0] + ".txt", "w") as f:
-    for profile in profiles:
-        X = np.array( [profile] )
-        prediction = trainer.model().predict(X)
-        chord_index = np.argmax(prediction)
+# trainer = Trainer()
+# trainer.load()
+
+# song = "about.wav"
+
+# longFileProfiler = LongFileProfiler("songs/" + song)
+# profiles = longFileProfiler.get_profile()
+
+# with open("songs_chords/" + song.split(".")[0] + ".txt", "w") as f:
+#     for profile in profiles:
+#         X = np.array( [profile] )
+#         prediction = trainer.model().predict(X)
+#         chord_index = np.argmax(prediction)
         
-        f.write( config()["pitches"][chord_index] + " " )
+#         f.write( config()["pitches"][chord_index] + " " )
 
