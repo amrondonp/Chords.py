e1945b623f56d82c58278b0a3db52301c0f2a73b
Andres Rondon <amrondonp@gmail.com>
2020-07-03 11:29:23 -0700

    Testing works!

--- Chords/ChordsTest/Profiling/ProfilingTest.cs
+++ Chords/ChordsTest/Profiling/ProfilingTest.cs
@@ -6,13 +6,21 @@ namespace ChordsTest.Profiling
     [TestClass]
     public class ProfilingTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        static bool CompareFloat(float a, float b)
         {
-            var tuple = Chords.Profiling.Profiling.GetSamples("./Resources/d.wav");
-            Assert.IsNotNull(tuple);
-            Assert.AreEqual(tuple.sampleRate, 44100);
-            Assert.AreEqual(tuple.samples.Length, 262144);
+            return Math.Abs(a - b) < 1e-5;
+        }
+
+        [TestMethod]
+        public void GetSamples_ReadsTheSamplesCorretly()
+        {
+            var (sampleRate, samples) = Chords.Profiling.Profiling.GetSamples("./Resources/d.wav");
+            Assert.IsNotNull(samples);
+            Assert.AreEqual(sampleRate, 44100);
+            Assert.AreEqual(samples.Length, 262144);
+            Assert.IsTrue(CompareFloat(samples[0], -0.00088501f));
+            Assert.IsTrue(CompareFloat(samples[1], -0.00082397f));
+            Assert.IsTrue(CompareFloat(samples[^1], -0.00204468f));
         }
     }
 }
