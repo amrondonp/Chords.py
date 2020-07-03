e985347432cf2e68ab66a0340cccd0c3cdc38879
Andres Rondon <amrondonp@gmail.com>
2020-07-03 12:37:41 -0700

    FFt working

--- Chords/Chords/Profiling/Profiling.cs
+++ Chords/Chords/Profiling/Profiling.cs
@@ -1,26 +1,38 @@
 ﻿using NAudio.Wave;
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Numerics;
+using MathNet.Numerics.IntegralTransforms;
 
 namespace Chords.Profiling
 {
     public class Profiling
     {
-        public static (int sampleRate, float [] samples) GetSamples(string pathToAudioFile)
+        public static (int sampleRate, float[] samples) GetSamples(string pathToAudioFile)
         {
             using AudioFileReader reader = new AudioFileReader(pathToAudioFile);
 
             var sampleProvider = reader.ToSampleProvider();
             float[] samples = new float[reader.Length / sizeof(float)];
             var samplesRead = sampleProvider.Read(samples, 0, samples.Length);
-            
-            if(samples.Length != samplesRead)
+
+            if (samples.Length != samplesRead)
             {
                 throw new Exception("Error when reading the samples, samples.Length=" + samples.Length + " samplesRead=" + samplesRead);
             }
 
             return (reader.WaveFormat.SampleRate, samples);
+        }
+
+        public static Complex[] GetFFT(float[] samples)
+        {
+            Complex[] complexData = new Complex[samples.Length];
+            for (int i = 0; i < samples.Length; i++)
+            {
+                complexData[i] = new Complex(samples[i], 0);
+            }
+
+            Fourier.Forward(complexData, FourierOptions.Matlab);
+            return complexData;
         }
     }
 }
--- Chords/ChordsTest/Profiling/ProfilingTest.cs
+++ Chords/ChordsTest/Profiling/ProfilingTest.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Numerics;
+
 
 namespace ChordsTest.Profiling
 {
@@ -9,6 +11,11 @@ namespace ChordsTest.Profiling
         static bool CompareFloat(float a, float b)
         {
             return Math.Abs(a - b) < 1e-5;
+        }
+
+        static bool CompareComplex(Complex complex, float x, float y)
+        {
+            return CompareFloat((float)complex.Real, x) && CompareFloat((float)complex.Imaginary, y);
         }
 
         [TestMethod]
@@ -21,6 +28,19 @@ namespace ChordsTest.Profiling
             Assert.IsTrue(CompareFloat(samples[0], -0.00088501f));
             Assert.IsTrue(CompareFloat(samples[1], -0.00082397f));
             Assert.IsTrue(CompareFloat(samples[^1], -0.00204468f));
+        }
+
+        [TestMethod]
+        public void GetFFT_ComputesTheFFTCorreclty()
+        {
+            var (_, samples) = Chords.Profiling.Profiling.GetSamples("./Resources/d.wav");
+            var fft = Chords.Profiling.Profiling.GetFFT(samples);
+            Assert.IsNotNull(fft);
+            Assert.AreEqual(fft.Length, 262144);
+            Assert.IsTrue(CompareComplex(fft[0], -12.335663f, 0.0f));
+            Assert.IsTrue(CompareComplex(fft[1], -12.377926f, 2.8002753f));
+            Assert.IsTrue(CompareComplex(fft[1000], -43.04428f, 24.787685f));
+            Assert.IsTrue(CompareComplex(fft[14000], 3.0144866f, -1.557055f));
         }
     }
 }
