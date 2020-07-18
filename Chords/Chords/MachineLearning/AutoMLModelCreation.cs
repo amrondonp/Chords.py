cb4119135fa9bbf100354ed82dcfd9d43a6e335a
Andres Mauricio Rondon Patino <amrondonp@gmail.com>
2020-07-17 21:30:33 -0700

    First attempt on AutoML

---
+++ Chords/Chords/MachineLearning/AutoMLModelCreation.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.AutoML;
+using Microsoft.ML.Data;
+
+namespace Chords.MachineLearning
+{
+    class AutoMLModelCreation
+    {
+        public static ExperimentResult<MulticlassClassificationMetrics> CreateModel()
+        {
+            MLContext mLContext = new MLContext();
+            var trainData = mLContext.Data.LoadFromTextFile<ChordData>(
+                "Resources/trainData.csv",
+                hasHeader: true,
+                separatorChar: ','
+            );
+
+            var experiment = mLContext.Auto().CreateMulticlassClassificationExperiment(10);
+            var result = experiment.Execute(trainData, "Chord");
+            return result;
+        }
+    }
+
+    internal class ChordData
+    {
+        [LoadColumn(0)]
+        public float C { get; set; }
+
+        [LoadColumn(1)]
+        public float CSharp { get; set; }
+
+        [LoadColumn(2)]
+        public float D { get; set; }
+
+        [LoadColumn(3)]
+        public float DSharp { get; set; }
+
+        [LoadColumn(4)]
+        public float E { get; set; }
+
+        [LoadColumn(5)]
+        public float F { get; set; }
+
+        [LoadColumn(6)]
+        public float FSharp { get; set; }
+
+        [LoadColumn(7)]
+        public float G { get; set; }
+
+        [LoadColumn(8)]
+        public float GSharp { get; set; }
+
+        [LoadColumn(9)]
+        public float A { get; set; }
+
+        [LoadColumn(10)]
+        public float ASharp { get; set; }
+
+        [LoadColumn(11)]
+        public float B { get; set; }
+
+        [LoadColumn(12), ColumnName("Chord")]
+        public float Chord { get; set; }
+    }
+}
