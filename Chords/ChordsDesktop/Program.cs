f0876486698239eb280bbc3bdcafde39288f9967
Andres Mauricio Rondon Patino <amrondonp@gmail.com>
2020-07-19 23:30:07 -0700

    Separate Chords from DesktopApp

---
+++ Chords/ChordsDesktop/Program.cs
@@ -0,0 +1,21 @@
+using Chords.Predictors;
+using System;
+using System.Windows.Forms;
+
+namespace ChordsDesktop
+{
+    static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1(new AutoMlPredictor()));
+        }
+    }
+}
