833abcb7e059345c1f2ff17e57ba10b6161f6e50
Andres Mauricio Rondon Patino <amrondonp@gmail.com>
2020-07-19 23:44:11 -0700

    Add chords data bag

---
+++ Chords/Chords/Entities/Chord.cs
@@ -0,0 +1,16 @@
+﻿namespace Chords.Entities
+{
+    public class Chord
+    {
+        public float[] Samples { get; }
+        public int SampleRate { get; }
+        public string Name { get; }
+
+        public Chord(float[] samples, int sampleRate, string name)
+        {
+            Name = name;
+            Samples = samples;
+            SampleRate = sampleRate;
+        }
+    }
+}
---
+++ Chords/ChordsTest/Entities/ChordTest.cs
@@ -0,0 +1,19 @@
+﻿using Chords.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChordsTest.Entities
+{
+    [TestClass]
+    public class ChordTest
+    {
+        [TestMethod]
+        public void Chord_IsCreated()
+        {
+            var samples = new[] { 1f, 2f, 3f };
+            var chord = new Chord(samples, 44100, "Em");
+            Assert.AreEqual(chord.Name, "Em");
+            Assert.AreEqual(chord.SampleRate, 44100);
+            Assert.AreEqual(chord.Samples, samples);
+        }
+    }
+}
