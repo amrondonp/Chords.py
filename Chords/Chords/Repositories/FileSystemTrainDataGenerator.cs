41edf0e6be30d045d0a82e361bbdee4f0fd38766
Andres Rondon <amrondonp@gmail.com>
2020-07-25 11:33:35 -0700

    TrainDataGenerator

---
+++ Chords/Chords/Repositories/FileSystemTrainDataGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Chords.Repositories
+{
+    public class FileSystemTrainDataGenerator
+    {
+        private readonly string inputDirectory;
+        private readonly string outputCsvFile;
+
+        public FileSystemTrainDataGenerator(string inputDirectory, string outputCsvFile)
+        {
+            this.inputDirectory = inputDirectory;
+            this.outputCsvFile = outputCsvFile; 
+        }
+
+        public void GenerateTrainData()
+        {
+            using StreamWriter sw = File.CreateText(outputCsvFile);
+            sw.WriteLine(fileHeader());
+        }
+
+        private string fileHeader()
+        {
+            return "c,c#,d,d#,e,f,f#,g,g#,a,a#,b,chord";
+        }
+    }
+}
---
+++ Chords/ChordsTest/Repository/FileSystemTrainDataGeneratorTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Chords.Repositories;
+using System.IO;
+
+namespace ChordsTest.Repository
+{   
+   [TestClass]
+    public class FileSystemTrainDataGeneratorTest
+    {
+        [TestMethod]
+        public void EmptyFolder_GeneratesCsvTrainDataEmpty()
+        {
+            const string inputDirectory = "./Resources/trainDataGeneratorFolder/";
+            const string outputCsvFile = inputDirectory + "trainDataGenerated.csv";
+
+            Assert.AreEqual(Directory.GetFiles(inputDirectory, "*.json").Length, 0);
+            Assert.IsFalse(File.Exists(outputCsvFile));
+
+            var trainDataGenerator = new FileSystemTrainDataGenerator(inputDirectory, outputCsvFile);
+            
+            try
+            {
+                trainDataGenerator.GenerateTrainData();
+
+                Assert.AreEqual(Directory.GetFiles(inputDirectory, "*.json").Length, 0);
+                Assert.IsTrue(File.Exists(outputCsvFile));
+
+                using StreamReader sr = File.OpenText(outputCsvFile);
+                string s = sr.ReadLine();
+
+                Assert.AreEqual(s, "c,c#,d,d#,e,f,f#,g,g#,a,a#,b,chord");
+            } finally
+            {
+                File.Delete(outputCsvFile);
+            }
+        }
+    }
+}
