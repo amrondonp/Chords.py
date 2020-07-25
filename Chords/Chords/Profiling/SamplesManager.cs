d4a8912c0b5e2a40f894eb295a0ad5cfb25a4100
Andres Mauricio Rondon Patino <amrondonp@gmail.com>
2020-07-24 23:14:17 -0700

    SamplesManager happy path

---
+++ Chords/Chords/Profiling/SamplesManager.cs
@@ -0,0 +1,27 @@
+﻿namespace Chords.Profiling
+{
+    public class SamplesManager
+    {
+        private readonly int sampleRate;
+        private readonly float[] samples;
+
+        public SamplesManager(int sampleRate, float[] samples)
+        {
+            this.sampleRate = sampleRate;
+            this.samples = samples;
+        }
+
+        public (int, float[]) GetSamplesAtPositionGivenWindowInMs(int position, int windowInMs)
+        {
+            var newSampleSize = LongAudioProfiling.GetNumberOfSamplesGivenWindowInMs(sampleRate, windowInMs);
+            var samplesReturned = new float[newSampleSize];
+
+            for (var i = 0; i < newSampleSize; i++)
+            {
+                samplesReturned[i] = samples[position * newSampleSize + i];
+            }
+
+            return (sampleRate, samplesReturned);
+        }
+    }
+}
---
+++ Chords/ChordsTest/Profiling/SamplesManagerTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChordsTest.Profiling
+{
+    [TestClass]
+    public class SamplesManagerTest
+    {
+        [TestMethod]
+        public void GetsTheChordsCorrectly()
+        {
+            var (sampleRate, samples) = Chords.Profiling.Profiling.GetSamples("./Resources/d.wav");
+            var samplesManager = new Chords.Profiling.SamplesManager(sampleRate, samples);
+            Assert.IsNotNull(samplesManager);
+
+            var (sampleRateReturned, samplesReturned) = samplesManager.GetSamplesAtPositionGivenWindowInMs(1, 500);
+            Assert.AreEqual(sampleRateReturned, sampleRate);
+            Assert.AreEqual(samplesReturned.Length, sampleRate / 2);
+            for(var i = 0 ; i < samplesReturned.Length; i++)
+            {
+                Assert.AreEqual(samplesReturned[i], samples[sampleRate / 2 + i]);
+            }
+        }
+    }
+}
