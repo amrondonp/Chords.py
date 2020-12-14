8d4732aebbf0feee135a304aba770f7ca4ad4aa6
Andres Mauricio Rondon Patino <amrondonp@gmail.com>
2020-12-13 17:37:21 -0800

    ConsoleApp v0.1

---
+++ Chords/ChordsConsole/Program.cs
@@ -0,0 +1,33 @@
+﻿using Chords.Predictors;
+using System;
+
+namespace ChordsConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = args.Length >= 1 ? args[0] : "./storedChords/big-in-japan.mp3";
+            IPredictor predictor = new AutoMlPredictor();
+            bool hasFinished = false;
+
+            var chordProcessingProgress = new Progress<int>(v =>
+            {
+                Console.WriteLine($@"Computing chords... {v} %");
+                if(v >= 100)
+                {
+                    hasFinished = true;
+                }
+            });
+
+            var predictions = predictor.GetPredictionForFile(file, chordProcessingProgress, 500);
+
+            while(!hasFinished) { }
+            
+            foreach(string prediction in predictions)
+            {
+                Console.Write(prediction + " ");
+            }
+        }
+    }
+}
