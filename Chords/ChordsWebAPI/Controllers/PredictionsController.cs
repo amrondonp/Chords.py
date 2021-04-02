7a27a908140f09d4e4d1ffb5a47d6aa63f0b872d
Andres Rondon <amrondonp@gmail.com>
2021-04-02 13:49:29 -0700

    api working

---
+++ Chords/ChordsWebAPI/Controllers/PredictionsController.cs
@@ -0,0 +1,28 @@
+﻿using ChordsWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChordsWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PredictionsController : Controller
+    {
+        private readonly PredictionContext _predictionContext;
+
+        public PredictionsController(PredictionContext predictionContext)
+        {
+            _predictionContext = predictionContext;
+        }
+
+        [HttpGet]
+        public async Task<List<Prediction>> GetAll()
+        {
+            return await _predictionContext.Predictions.ToListAsync();
+        }
+    }
+}
