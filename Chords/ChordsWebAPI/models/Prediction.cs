b632233de69d53f5134421a79f830328be699b51
Andres Rondon <amrondonp@gmail.com>
2021-04-02 12:55:33 -0700

    model

---
+++ Chords/ChordsWebAPI/models/Prediction.cs
@@ -0,0 +1,15 @@
+﻿using Chords.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChordsWebAPI.models
+{
+    public class Prediction
+    {
+        public int Id { get; set; }
+        public int Progress { get; set; }
+        public Chord[] Chords { get; set; }
+    }
+}
