031714e143fec8eee768a9a9a6081130cedb4ea8
Andres Rondon <amrondonp@gmail.com>
2021-04-03 11:31:07 -0700

    file sorta working

--- chords-frontend/src/NewPrediction.tsx
+++ chords-frontend/src/NewPrediction.tsx
@@ -1,11 +1,32 @@
 import styles from "./NewPrediction.module.css";
-import { PrimaryButton } from "@fluentui/react";
+import { PrimaryButton, DefaultButton } from "@fluentui/react";
+import React, { ChangeEvent } from "react";
 
 export function NewPrediction() {
+  const [file, setFile] = React.useState<File | null>(null);
+  const inputFile = React.useRef<HTMLInputElement | null>(null);
+  const onClick = () => {
+    inputFile.current?.click();
+  };
+
+  const onChangeFile = (event: ChangeEvent<HTMLInputElement>) => {
+    setFile(event.target?.files && event.target?.files[0]);
+  };
+
+  const onFileCommitted = () => {};
+
   return (
     <div className={styles.container}>
-      <span>Upload a song to get the chords</span>
-      <PrimaryButton text="Primary" onClick={() => {}} />
+      <input
+        type="file"
+        id="file"
+        ref={inputFile}
+        style={{ display: "none" }}
+        onChange={onChangeFile}
+      />
+      <span>{file ? file.name : "No file selected"}</span>
+      <DefaultButton text="Select File" onClick={onClick} />
+      <PrimaryButton text="Get the chords" onClick={onFileCommitted} />
     </div>
   );
 }
