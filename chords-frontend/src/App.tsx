8260e288bd130b514f91da7527e923f8d48ca2f5
Andres Rondon <amrondonp@gmail.com>
2021-04-04 00:52:48 -0700

    Themes!

--- chords-frontend/src/App.tsx
+++ chords-frontend/src/App.tsx
@@ -1,15 +1,43 @@
 import { PredictionView } from "./Prediction";
 import { Predictions } from "./Predictions";
 import { NewPrediction } from "./NewPrediction";
-import styles from "./App.module.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { ThemeProvider } from "@fluentui/react";
-import { guitarTheme } from "./Themes";
+import { ThemeProvider, Toggle } from "@fluentui/react";
+import { blueTheme, guitarTheme } from "./Themes";
+
+import styles from "./App.module.css";
+import "./Theme.css";
+import React from "react";
 
 function App() {
+  const [isGuitarTheme, setIsGuitarTheme] = React.useState(true);
+
+  React.useEffect(() => {
+    document.documentElement.setAttribute(
+      "data-theme",
+      isGuitarTheme ? "guitar" : "blue"
+    );
+  }, [isGuitarTheme]);
+
   return (
-    <ThemeProvider applyTo="body" theme={guitarTheme}>
+    <ThemeProvider
+      applyTo="body"
+      theme={isGuitarTheme ? guitarTheme : blueTheme}
+    >
       <Router>
+        <div className={styles.navBar}>
+          <h3>Chords</h3>
+          <Toggle
+            label="Theme"
+            onText="guitar"
+            offText="blue"
+            inlineLabel
+            onChange={(_: any, checked?: boolean) =>
+              setIsGuitarTheme(checked ? true : false)
+            }
+            checked={isGuitarTheme}
+          />
+        </div>
         <div className={styles.appContainer}>
           <Switch>
             <Route path="/predictions/:id">
