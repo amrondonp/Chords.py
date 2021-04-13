de4ed25d6fb5a98fa22685f4810b286c6a7b0c75
Andres Rondon <amrondonp@gmail.com>
2021-04-12 21:58:51 -0700

    sorta working

---
+++ Chords/ChordsWeb/ClientApp/src/Themes.ts
@@ -0,0 +1,55 @@
+import { createTheme } from "@fluentui/react";
+
+export const guitarTheme = createTheme({
+  palette: {
+    themePrimary: '#643930',
+    themeLighterAlt: '#f9f4f4',
+    themeLighter: '#e6d6d3',
+    themeLight: '#d0b5b0',
+    themeTertiary: '#a2786f',
+    themeSecondary: '#764940',
+    themeDarkAlt: '#5a332b',
+    themeDark: '#4c2b24',
+    themeDarker: '#38201b',
+    neutralLighterAlt: '#faf9f8',
+    neutralLighter: '#f3f2f1',
+    neutralLight: '#edebe9',
+    neutralQuaternaryAlt: '#e1dfdd',
+    neutralQuaternary: '#d0d0d0',
+    neutralTertiaryAlt: '#c8c6c4',
+    neutralTertiary: '#a19f9d',
+    neutralSecondary: '#605e5c',
+    neutralPrimaryAlt: '#3b3a39',
+    neutralPrimary: '#323130',
+    neutralDark: '#201f1e',
+    black: '#000000',
+    white: '#ffffff',
+  }
+});
+
+export const blueTheme = createTheme({
+  palette: {
+    themePrimary: '#0078d4',
+    themeLighterAlt: '#eff6fc',
+    themeLighter: '#deecf9',
+    themeLight: '#c7e0f4',
+    themeTertiary: '#71afe5',
+    themeSecondary: '#2b88d8',
+    themeDarkAlt: '#106ebe',
+    themeDark: '#005a9e',
+    themeDarker: '#004578',
+    neutralLighterAlt: '#faf9f8',
+    neutralLighter: '#f3f2f1',
+    neutralLight: '#edebe9',
+    neutralQuaternaryAlt: '#e1dfdd',
+    neutralQuaternary: '#d0d0d0',
+    neutralTertiaryAlt: '#c8c6c4',
+    neutralTertiary: '#a19f9d',
+    neutralSecondary: '#605e5c',
+    neutralPrimaryAlt: '#3b3a39',
+    neutralPrimary: '#323130',
+    neutralDark: '#201f1e',
+    black: '#000000',
+    white: '#ffffff',
+  }
+})
